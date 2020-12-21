92ad7553e3992217f005836a58491f46c0feb466
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-12-21 01:40:42 +0100

    update version

--- src/main/java/com/supermartijn642/simplemagnets/SimpleMagnets.java
+++ src/main/java/com/supermartijn642/simplemagnets/SimpleMagnets.java
@@ -24,7 +24,7 @@ public class SimpleMagnets {
 
     public static final String MODID = "simplemagnets";
     public static final String NAME = "Simple Magnets";
-    public static final String VERSION = "1.0.8a";
+    public static final String VERSION = "1.0.8b";
 
     @Mod.Instance
     public static SimpleMagnets instance;
