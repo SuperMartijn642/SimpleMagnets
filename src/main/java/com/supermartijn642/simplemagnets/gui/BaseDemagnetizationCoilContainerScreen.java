70da9f59bd51955b5b7fbffe131e32bda890d316
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-02-21 18:10:59 +0100

    Update Config Lib Add demagnetization coils Add a durability button to the advanced magnet's filter

---
+++ src/main/java/com/supermartijn642/simplemagnets/gui/BaseDemagnetizationCoilContainerScreen.java
@@ -0,0 +1,91 @@
+package com.supermartijn642.simplemagnets.gui;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.supermartijn642.simplemagnets.DemagnetizationCoilTile;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+/**
+ * Created 7/7/2020 by SuperMartijn642
+ */
+public abstract class BaseDemagnetizationCoilContainerScreen<T extends BaseDemagnetizationCoilContainer> extends ContainerScreen<T> {
+
+    public BaseDemagnetizationCoilContainerScreen(T container, String title){
+        super(container, container.player.inventory, new TranslationTextComponent(title));
+        this.xSize = container.width;
+        this.ySize = container.height;
+    }
+
+    @Override
+    protected void init(){
+        super.init();
+
+        DemagnetizationCoilTile tile = this.container.getTileOrClose();
+        if(tile != null)
+            this.addButtons(tile);
+    }
+
+    protected abstract void addButtons(DemagnetizationCoilTile tile);
+
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks){
+        this.renderBackground(matrixStack);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
+        this.func_230459_a_(matrixStack, mouseX, mouseY);
+
+        DemagnetizationCoilTile tile = this.container.getTileOrClose();
+        if(tile != null)
+            this.drawToolTips(matrixStack, tile, mouseX, mouseY);
+    }
+
+    protected abstract void drawToolTips(MatrixStack matrixStack, DemagnetizationCoilTile tile, int mouseX, int mouseY);
+
+    @Override
+    public void tick(){
+        DemagnetizationCoilTile tile = this.container.getTileOrClose();
+        if(tile == null)
+            return;
+
+        super.tick();
+        this.tick(tile);
+    }
+
+    protected abstract void tick(DemagnetizationCoilTile tile);
+
+    protected abstract String getBackground();
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY){
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("simplemagnets", "textures/" + this.getBackground()));
+        this.blit(matrixStack, this.guiLeft, this.guiTop, 0, 0, this.xSize, this.ySize);
+
+        DemagnetizationCoilTile tile = this.container.getTileOrClose();
+        if(tile != null)
+            this.drawText(matrixStack, tile);
+    }
+
+    protected abstract void drawText(MatrixStack matrixStack, DemagnetizationCoilTile tile);
+
+    public void drawCenteredString(MatrixStack matrixStack, ITextComponent text, float x, float y){
+        this.font.func_243248_b(matrixStack, text, this.guiLeft + x - this.font.func_238414_a_(text) / 2f, this.guiTop + y, 4210752);
+    }
+
+    public void drawString(MatrixStack matrixStack, ITextComponent text, float x, float y){
+        this.font.func_243248_b(matrixStack, text, this.guiLeft + x, this.guiTop + y, 4210752);
+    }
+
+    public void renderToolTip(MatrixStack matrixStack, boolean translate, String string, int x, int y){
+        super.renderTooltip(matrixStack, translate ? new TranslationTextComponent(string) : new StringTextComponent(string), x, y);
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(MatrixStack p_230451_1_, int p_230451_2_, int p_230451_3_){
+        // stop default text drawing
+    }
+}
---
+++ src/main/java/com/supermartijn642/simplemagnets/gui/FilteredDemagnetizationCoilContainerScreen.java
@@ -0,0 +1,81 @@
+package com.supermartijn642.simplemagnets.gui;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.supermartijn642.simplemagnets.DemagnetizationCoilTile;
+import com.supermartijn642.simplemagnets.SimpleMagnets;
+import com.supermartijn642.simplemagnets.packets.demagnetization_coil.*;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+/**
+ * Created 7/15/2020 by SuperMartijn642
+ */
+public class FilteredDemagnetizationCoilContainerScreen extends BaseDemagnetizationCoilContainerScreen<FilteredDemagnetizationCoilContainer> {
+
+    private UpDownArrowButton upXButton, downXButton;
+    private UpDownArrowButton upYButton, downYButton;
+    private UpDownArrowButton upZButton, downZButton;
+    private WhitelistButton whitelistButton;
+    private DurabilityButton durabilityButton;
+
+    public FilteredDemagnetizationCoilContainerScreen(FilteredDemagnetizationCoilContainer container){
+        super(container, container.getTileOrClose().getBlockState().getBlock().getTranslationKey());
+    }
+
+    @Override
+    protected void addButtons(DemagnetizationCoilTile tile){
+        this.upXButton = this.addButton(new UpDownArrowButton(this.guiLeft + 40, this.guiTop + 37, false, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketIncreaseXRange(this.container.pos))));
+        this.downXButton = this.addButton(new UpDownArrowButton(this.guiLeft + 40, this.guiTop + 63, true, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketDecreaseXRange(this.container.pos))));
+        this.upYButton = this.addButton(new UpDownArrowButton(this.guiLeft + 93, this.guiTop + 37, false, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketIncreaseYRange(this.container.pos))));
+        this.downYButton = this.addButton(new UpDownArrowButton(this.guiLeft + 93, this.guiTop + 63, true, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketDecreaseYRange(this.container.pos))));
+        this.upZButton = this.addButton(new UpDownArrowButton(this.guiLeft + 146, this.guiTop + 37, false, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketIncreaseZRange(this.container.pos))));
+        this.downZButton = this.addButton(new UpDownArrowButton(this.guiLeft + 146, this.guiTop + 63, true, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketDecreaseZRange(this.container.pos))));
+        this.whitelistButton = this.addButton(new WhitelistButton(this.guiLeft + 175, this.guiTop + 88, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketToggleWhitelist(this.container.pos))));
+        this.whitelistButton.update(tile.filterWhitelist);
+        this.durabilityButton = this.addButton(new DurabilityButton(this.guiLeft + 197, this.guiTop + 88, () -> SimpleMagnets.CHANNEL.sendToServer(new PacketToggleDurability(this.container.pos))));
+        this.durabilityButton.update(tile.filterDurability);
+    }
+
+    @Override
+    protected void drawToolTips(MatrixStack matrixStack, DemagnetizationCoilTile tile, int mouseX, int mouseY){
+        if(this.upXButton.isHovered() || this.upYButton.isHovered() || this.upZButton.isHovered())
+            this.renderToolTip(matrixStack, true, "gui.simplemagnets.demagnetization_coil.range.increase", mouseX, mouseY);
+        if(this.downXButton.isHovered() || this.downYButton.isHovered() || this.downZButton.isHovered())
+            this.renderToolTip(matrixStack, true, "gui.simplemagnets.demagnetization_coil.range.decrease", mouseX, mouseY);
+        if(this.whitelistButton.isHovered())
+            this.renderToolTip(matrixStack, true, "gui.advancedmagnet.whitelist." + (tile.filterWhitelist ? "on" : "off"), mouseX, mouseY);
+        if(this.durabilityButton.isHovered())
+            this.renderToolTip(matrixStack, true, "gui.simplemagnets.demagnetization_coil.durability." + (tile.filterDurability ? "on" : "off"), mouseX, mouseY);
+    }
+
+    @Override
+    protected void tick(DemagnetizationCoilTile tile){
+        this.whitelistButton.update(tile.filterWhitelist);
+        this.durabilityButton.update(tile.filterDurability);
+    }
+
+    @Override
+    protected String getBackground(){
+        return "filtered_demagnetization_coil_screen.png";
+    }
+
+    @Override
+    protected void drawText(MatrixStack matrixStack, DemagnetizationCoilTile tile){
+        this.drawCenteredString(matrixStack, this.title, this.xSize / 2f, 6);
+        this.drawString(matrixStack, this.playerInventory.getDisplayName(), 32, 112);
+
+        String range = I18n.format("gui.simplemagnets.demagnetization_coil.range")
+            .replace("$numberx$", "" + ((tile.rangeX - 1) * 2 + 1))
+            .replace("$numbery$", "" + ((tile.rangeY - 1) * 2 + 1))
+            .replace("$numberz$", "" + ((tile.rangeZ - 1) * 2 + 1));
+        this.drawString(matrixStack, new StringTextComponent(range), 8, 26);
+        this.drawCenteredString(matrixStack, new StringTextComponent("x:"), 35, 51);
+        this.drawCenteredString(matrixStack, new StringTextComponent("" + tile.rangeX), 49, 52);
+        this.drawCenteredString(matrixStack, new StringTextComponent("y:"), 88, 51);
+        this.drawCenteredString(matrixStack, new StringTextComponent("" + tile.rangeY), 102, 52);
+        this.drawCenteredString(matrixStack, new StringTextComponent("z:"), 141, 51);
+        this.drawCenteredString(matrixStack, new StringTextComponent("" + tile.rangeZ), 155, 52);
+        this.drawString(matrixStack, new TranslationTextComponent("gui.advancedmagnet.filter"), 8, 78);
+    }
+}
