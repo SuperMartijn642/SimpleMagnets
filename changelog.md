71cfac1de5df4c399d92e0e58d1a977aea93d958
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-01-01 21:48:06 +0100

    Update version and prevent spectators from pulling in items #37

---
+++ changelog.md
@@ -0,0 +1,3 @@
+### Simple Magnets 1.1.9
+- Updated to core library 1.1
+- Fix magnets pulling in items when in spectator mode
--- src/main/java/com/supermartijn642/simplemagnets/MagnetItem.java
+++ src/main/java/com/supermartijn642/simplemagnets/MagnetItem.java
@@ -55,6 +55,10 @@ public abstract class MagnetItem extends BaseItem {
 
     @Override
     public void inventoryUpdate(ItemStack stack, World level, Entity entity, int itemSlot, boolean isSelected){
+        // Prevent spectators from picking up items
+        if(entity.isSpectator())
+            return;
+
         CompoundNBT tag = stack.getOrCreateTag();
         if(tag.contains("active") && tag.getBoolean("active")){
             if(this.canPickupItems(tag)){
