411700d412156d7706c5018f175205d4093cbd0a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-02-26 20:19:08 +0100

    Port to Fabric 1.19

--- src/main/java/com/supermartijn642/simplemagnets/mixin/LevelRendererMixin.java
+++ src/main/java/com/supermartijn642/simplemagnets/mixin/LevelRendererMixin.java
@@ -1,7 +1,6 @@
 package com.supermartijn642.simplemagnets.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Matrix4f;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.simplemagnets.DemagnetizationCoilAreaHighlighter;
 import net.minecraft.client.Camera;
@@ -10,6 +9,7 @@ import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
