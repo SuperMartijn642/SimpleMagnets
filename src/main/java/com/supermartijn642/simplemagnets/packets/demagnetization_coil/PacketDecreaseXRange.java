62a4604ef61a12d1decaa13a8b53c077f2bc7172
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-18 22:20:07 +0200

    Enforce `#markDirty` when updating demagnetization coil options

--- src/main/java/com/supermartijn642/simplemagnets/packets/demagnetization_coil/PacketDecreaseXRange.java
+++ src/main/java/com/supermartijn642/simplemagnets/packets/demagnetization_coil/PacketDecreaseXRange.java
@@ -19,6 +19,6 @@ public class PacketDecreaseXRange extends BlockEntityBasePacket<DemagnetizationC
 
     @Override
     protected void handle(DemagnetizationCoilBlockEntity entity, PacketContext context){
-        entity.setRangeX(entity.rangeX - 1);
+        entity.setRangeX(entity.getRangeX() - 1);
     }
 }
