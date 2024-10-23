857acd7cfc1f33e76a02a381eb75658dadb7002e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-10-23 23:55:49 +0200

    Update to 1.21.2

--- src/main/java/com/supermartijn642/simplemagnets/mixin/LevelRendererMixin.java
+++ src/main/java/com/supermartijn642/simplemagnets/mixin/LevelRendererMixin.java
@@ -3,7 +3,9 @@ package com.supermartijn642.simplemagnets.mixin;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.simplemagnets.DemagnetizationCoilAreaHighlighter;
+import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,19 +21,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LevelRendererMixin {
 
     @Unique
-    private static PoseStack POSE_STACK = new PoseStack();
+    private static final PoseStack POSE_STACK = new PoseStack();
 
     @Inject(
-        method = "renderLevel",
+        method = "renderBlockOutline(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;Lcom/mojang/blaze3d/vertex/PoseStack;Z)V",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/phys/BlockHitResult;getBlockPos()Lnet/minecraft/core/BlockPos;",
             shift = At.Shift.AFTER
         )
     )
-    private void renderLevel(CallbackInfo ci){
+    private void renderLevel(Camera camera, MultiBufferSource.BufferSource bufferSource, PoseStack poseStack, boolean bl, CallbackInfo ci){
         HitResult hitResult = ClientUtils.getMinecraft().hitResult;
-        if(hitResult instanceof BlockHitResult)
+        if(bl && hitResult instanceof BlockHitResult && hitResult.getType() != HitResult.Type.MISS)
             DemagnetizationCoilAreaHighlighter.onDrawHighlight(POSE_STACK, ((BlockHitResult)hitResult));
     }
 }
